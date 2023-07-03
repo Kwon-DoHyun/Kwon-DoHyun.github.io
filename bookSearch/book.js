e2a26fe9ad07e0a8765781973bc51f7c7b88bac7
Kwon-DoHyun <121049127+Kwon-DoHyun@users.noreply.github.com>
2023-07-03 21:18:41 +0900

    Update book.js

--- bookSearch/book.js
+++ bookSearch/book.js
@@ -1,96 +1,139 @@
+// API : Application Programming Interface
+
 
 let page = 1;
-let pagesize = 50;
-
+let size = 20;
+const container = document.querySelector(".container");
 const query = document.querySelector(".query");
-//
-
-const searchBox = document.querySelector(".search-box");
-searchBox.addEventListener("submit", e => {
+const searchBox = document.querySelector(".search_box");
+searchBox.addEventListener("submit", e =>{
     e.preventDefault();
-    if (query !== "") {
-        page = 1;
+    if(query !== ""){
+        page = 1
         searchRequest(query.value, page);
     }
     query.value = "";
+    
 })
 
-function searchRequest(query, page) {
-    console.log("query : ", query);
-    // url : client가 요청을 보낼 SERVER URL 주소
-    // .done : 요청 성공시 받아온 요청 데이터가 done() 메소드로 전달
-    $.ajax({
-        "url": `https://dapi.kakao.com/v3/search/book?query=${query}&page=${page}&size=${pagesize}&target=title`,
-        "method": "GET",
-        "timeout": 0,
-        "headers": {
-            "Authorization": "KakaoAK f38b9fe2c66283c909a3b11ba777576c"
-        }
-    }).done(function (response) {
+function searchRequest(query){
+    console.log("query: ", query);
+$.ajax({
+      "url": `https://dapi.kakao.com/v3/search/book?query=${query}&page=${page}&size=${size}&target=title`,
+      "method": "GET",
+      "timeout": 0,
+      "headers": {
+        "Authorization": "KakaoAK 41c980bef2354a383155d4e7b7be48fa"
+      },
+    })
+    .done((response) =>{
+        console.log(response);
 
-
-        // 검색창 부분
         $(".container").empty();
-        let contents = '';
-        for (let i = 0; i < response.documents.length; i++) {
-            let object = response.documents[i];
-            if (object.thumbnail !== "") {
-                contents += '<div class="result-card">';
-                contents += '<a href="' + object.url + '">';
-                contents += '<div class="img"><img class="book-img" src="' + object.thumbnail + '" alt=""></div>';
-                contents += '<h4 class="book-title">' + object.title + '</h4>';
-                contents += '</a>';
-                contents += '<span class="price">정가 : ' + object.price + '원</span>';
-                contents += '<p class="book-info">';
-                contents += '<span class="author">' + object.authors + '</span>';
-                contents += ' | <span class="publisher">' + object.publisher + '</span></p></div>';
+        for(let i=0;i<response.documents.length;i++){
+            const thumbnail = response.documents[i].thumbnail;
+            const title = response.documents[i].title;
+            const description = response.documents[i].contents;
+            const price = response.documents[i].price;
+            const sale_price = response.documents[i].sale_price;
+            const authors = response.documents[i].authors;
+            const publisher = response.documents[i].publisher;
+            
+            const box = document.createElement("div");
+            box.setAttribute("class","result_card");
+           
+            const img = document.createElement("img");
+            const h4 = document.createElement("h4");
+            const p = document.createElement("p");
+            const span = document.createElement("span");
+            const sale = document.createElement("span");
+            const bunri = document.createElement("span");
+            const tempDiv = document.createElement("div");
+            const tempP = document.createElement("p");
+            const pAuth = document.createElement("span");
+            const pPubl = document.createElement("span")
+            const bunri2 = document.createElement("span");
+            
+            img.setAttribute("class","book_img");
+            img.setAttribute("src",`${thumbnail}`);
+            h4.setAttribute("class","book_title");
+            p.setAttribute("class","book_description");
+            span.setAttribute("class","price");
+            sale.setAttribute("class","sale_price");
+            pAuth.setAttribute("class","authors");
+            pPubl.setAttribute("class","publisher");
+            tempP.setAttribute("class","tempP");
+            if(sale_price === -1){
+                sale.setAttribute("style","display : none");
+                span.setAttribute("style","text-decoration : none");
+                bunri.setAttribute("style","display:none")
             }
+            if(thumbnail === ""){
+                box.setAttribute("style","display:none");
+            }
+
+            h4.innerText =`${title}`;
+            p.innerText = `${description}`;
+            span.innerText = `${price}원`;
+            bunri.innerText = " | "
+            bunri2.innerText = " | "
+            sale.innerText = `${sale_price}원`;
+            pAuth.innerText = `저자 : ${authors}`;
+            pPubl.innerText = `제작사 : ${publisher}`;
+
+            
+            container.append(box);
+            box.append(img);
+            box.append(h4);
+            box.append(p);
+            box.append(tempDiv);
+            tempDiv.append(span);
+            tempDiv.append(bunri);
+            tempDiv.append(sale);
+            box.append(tempP);
+            tempP.append(pAuth);
+            tempP.append(bunri2);
+            tempP.append(pPubl);
+
         }
-        $(".container").append(contents);
 
-
-        // 페이징 부분
-        
         $(".paging").empty();
-        let pageContent = '';
-        let count = response.meta.pageable_count; // 검색한 결과 수 
-        // 검색한 결과 수에 따른 최대 검색페이지 수
-        // Math.ceil = 올림 (1.007) => 2
-        let pageCount = Math.ceil(count / pagesize); 
-
-        pageContent += '<img ';
-        if (page > 1) {
-            pageContent += 'class = "before"';
-            pageContent += 'src="resources/left_arrow.png"';
+        let pageLength = response.meta.pageable_count;
+        let pageCount = Math.ceil(pageLength/size);
+        let pageContents = "";
+        pageContents += '<img'; 
+        if(page > 1){
+        pageContents += ' class="before" src="images/before.png"';
         }
-        pageContent += '>';
-        pageContent += '<span>' + page + ' / ' + pageCount + '</span>';
-        pageContent += '<img ';
+        pageContents += '>';
+        pageContents += '<span>' +page + '/' + pageCount+'</span>';
+
+        pageContents += '<img'; 
         if(response.meta.is_end === false){
-            pageContent += 'class = "after"'; 
-            pageContent += 'src="resources/right_arrow.png"';
+            pageContents += ' class="after" src="images/after.png"';
         }
-        pageContent += '>';
-        $(".paging").append(pageContent);
+        pageContents += '>';
+        $(".paging").append(pageContents);
 
-        // 페이지 이동 이벤트리스너
-        if( page > 1){
+        if(page > 1){
             const before = document.querySelector(".before");
-            before.addEventListener("click", e => {
+            before.addEventListener("click",e =>{
                 page--;
                 searchRequest(query, page);
             })
         }
-        if( response.meta.is_end === false){
+        if(response.meta.is_end === false){
             const after = document.querySelector(".after");
-            after.addEventListener("click", e => {
+            after.addEventListener("click",e =>{
                 page++;
                 searchRequest(query, page);
             })
         }
-        console.log(response);
+
+
     });
 }
+
 
 
 
